78f886a0720a2eb77ed41ab8348960ea09bbb53e
Alejandro <amazzuca@hotmail.com>
2018-11-12 18:16:42 +0000

    actualizar spider.py
    No estaba funcionando el método de paginación ya que el sitio cambió el codigo.
    Se corrigió el link y metodo de paginación
    se arreglaron los links de: modelo, marca, tecnología, tipo y ventas_vendedor

--- mercado/spiders/spider.py
+++ mercado/spiders/spider.py
@@ -16,19 +16,19 @@ class MercadoSpider(CrawlSpider):
 
 	rules = {
 		# Para cada item
-		Rule(LinkExtractor(allow = (), restrict_xpaths = ('//li[@class="pagination__next"]/a'))),
+		Rule(LinkExtractor(allow =['Desde'] )),
 		Rule(LinkExtractor(allow =(), restrict_xpaths = ('//h2[contains(@class,"item__title")]/a')),
-							callback = 'parse_item', follow = False)
+							callback = 'parse_item', follow = True)
 	}
 
 	def parse_item(self, response):
 		ml_item = MercadoItem()
 		#info de producto
 		ml_item['titulo'] = response.xpath('normalize-space(//h1[@class="item-title__primary "]/text())').extract_first()
-		ml_item['modelo'] = response.xpath('normalize-space(/html/body/main/div/div/div[1]/div[1]/section[1]/div/section[1]/ul/li[1]/span)').extract()
-		ml_item['marca'] = response.xpath('normalize-space(/html/body/main/div/div/div[1]/div[1]/section[1]/div/section[1]/ul/li[2]/span)').extract()
-		ml_item['tecnologia'] = response.xpath('normalize-space(/html/body/main/div/div/div[1]/div[1]/section[1]/div/section[2]/ul/li[1]/span)').extract()
-		ml_item['tipo'] = response.xpath('normalize-space(/html/body/main/div/div/div[1]/div[1]/section[1]/div/section[2]/ul/li[2]/span)').extract()
+		ml_item['modelo'] = response.xpath('normalize-space(/html/body/main/div/div[1]/div[1]/section[3]/div/section/ul/li[3]/span)').extract()
+		ml_item['marca'] = response.xpath('normalize-space(/html/body/main/div/div[1]/div[1]/section[3]/div/section/ul/li[1]/span)').extract()
+		ml_item['tecnologia'] = response.xpath('normalize-space(/html/body/main/div/div[1]/div[1]/section[3]/div/section/ul/li[5]/span)').extract()
+		ml_item['tipo'] = response.xpath('normalize-space(/html/body/main/div/div[1]/div[1]/section[3]/div/section/ul/li[6]/span)').extract()
 		ml_item['precio'] = response.xpath('normalize-space(//span[@class="price-tag-fraction"]/text())').extract()
 		ml_item['condicion'] = response.xpath('normalize-space(//div[@class="item-conditions"]/text())').extract()
 		ml_item['envio'] = response.xpath('normalize-space(//p[contains(@class, "shipping-method-title")]/text())').extract()
@@ -40,7 +40,7 @@ class MercadoSpider(CrawlSpider):
 		#info de la tienda o vendedor
 		ml_item['vendedor_url'] = response.xpath('//*[contains(@class, "reputation-view-more")]/@href').extract()
 		ml_item['tipo_vendedor'] = response.xpath('normalize-space(//p[contains(@class, "power-seller")]/text())').extract()
-		ml_item['ventas_vendedor'] = response.xpath('normalize-space(//div[@class="feedback-title"]/text())').extract()
+		ml_item['ventas_vendedor'] = response.xpath('normalize-space(/html/body/main/div/div[1]/div[2]/div[1]/section[2]/div[4]/dl/dd[1]/strong)').extract()
 		self.item_count += 1
 		if self.item_count > 5:
 			raise CloseSpider('item_exceeded')
